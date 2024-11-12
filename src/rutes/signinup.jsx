a0cac21b126ebfb0fe560b882f6d364ea9895293
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2024-11-12 16:17:59 +0100

    more style

--- src/rutes/signinup.jsx
+++ src/rutes/signinup.jsx
@@ -84,10 +84,10 @@ export default function Sign() {
                     <form onSubmit={signIn} className="signForm">
                         <h1>Sign in</h1>
                         <label htmlFor={usernameSignInId}>Username</label>
-                        <input type="text" name="username_signin" value={username_si} onChange={(e) => setUserame_si(e.target.value)} id={usernameSignInId} />
+                        <input type="text" autoComplete="off" name="username_signin" value={username_si} onChange={(e) => setUserame_si(e.target.value)} id={usernameSignInId} />
 
                         <label htmlFor={passwordSignInId}>Password</label>
-                        <input type="password" name="password_signin" value={password_si} onChange={(e) => setPassword_si(e.target.value)} id={passwordSignInId} />
+                        <input type="password" autoComplete="off" name="password_signin" value={password_si} onChange={(e) => setPassword_si(e.target.value)} id={passwordSignInId} />
 
                         <button type="submit" className="mainButton">Sign in</button>
                     </form>
@@ -95,13 +95,13 @@ export default function Sign() {
                     <form onSubmit={signUp} className="signForm">
                         <h1>Sign up</h1>
                         <label htmlFor={nameId}>Name</label>
-                        <input type="text" name="name_signup" value={name_su} onChange={(e) => setName(e.target.value)} id={nameId} />
+                        <input type="text" autoComplete="off" name="name_signup" value={name_su} onChange={(e) => setName(e.target.value)} id={nameId} />
 
                         <label htmlFor={usernameSignUpId}>Username</label>
-                        <input type="text" name="username_signup" value={username_su} onChange={(e) => setUserame_su(e.target.value)} id={usernameSignUpId} />
+                        <input type="text" autoComplete="off" name="username_signup" value={username_su} onChange={(e) => setUserame_su(e.target.value)} id={usernameSignUpId} />
 
                         <label htmlFor={passwordSignUpId}>Password</label>
-                        <input type="password" name="password_signup" value={password_su} onChange={(e) => setPassword_su(e.target.value)} id={passwordSignUpId} />
+                        <input type="password" autoComplete="off" name="password_signup" value={password_su} onChange={(e) => setPassword_su(e.target.value)} id={passwordSignUpId} />
 
                         <button type="submit" className="secondaryButton">Sign up</button>
                     </form>
