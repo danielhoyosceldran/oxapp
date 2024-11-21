4fe17b26a4e96822aa33f05996e7bec9be013bc1
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2024-11-21 17:23:09 +0100

    working on landing page

--- src/Oxapp.jsx
+++ src/Oxapp.jsx
@@ -3,8 +3,6 @@ import { useAuth } from './auth/AuthProvider'
 import Chat from './pages/p-chat';
 import LandingPage from './pages/p-landingPage.jsx';
 
-import './styles/global/s-oxapp.css'
-
 function App() {
   const auth = useAuth();
   return auth.isAuthenticated ? <Chat /> : <LandingPage />;
--- src/pages/p-landingPage.jsx
+++ src/pages/p-landingPage.jsx
@@ -1,17 +1,36 @@
 import { Link } from 'react-router-dom';
 
+import Next from "../assets/icons/next-dm.svg";
+import Logo from "../assets/logo/transparent_logo.png"
+
 import "../styles/landingPage/s-landingPage.css"
+import "../styles/landingPage/s-aside.css"
+import "../styles/landingPage/s-footer.css"
 
 export default function LandingPage() {
     return (
-        <>
-            <div className='lp-body'>
-                <header className='lp-header'>
-                    <div className='g-pointer mainButton lp-header-element'>
-                        <Link to="/access">access</Link>
+        <div className='lp-body'>
+            <div>
+                <aside className='lp-aside g-flex g-horizonal-center-flex g-vertical-center-flex'>
+                    <div className='g-pointer'>
+                        <Link to="/access"><img className='lp-aside-img' src={Next} alt="" /></Link>
                     </div>
-                </header>
+                </aside>
+                <div className='lp-content-container'>
+                    <div className='lp-content-title'>
+                        <div className='g-flex g-horizonal-center-flex g-vertical-center-flex'>
+                            <img src={Logo} alt="" />
+                            <h1>OXAPP</h1>
+                        </div>
+                    </div>
+                    <section className='lp-content-about'>
+
+                    </section>
+                </div>
             </div>
-        </>
+            <footer className='lp-footer'>
+
+            </footer>
+        </div>
     );
 }
