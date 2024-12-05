7414a27c3668f0c663d41317b22985d2a13689bf
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2024-12-05 17:03:28 +0100

    theme done. forgot one thing

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,24 +1,24 @@
-import { useContext, createContext, useState } from "react";
-import { blankRequest } from "../api_handlers/session";
-import { API_URL, LOGGED_IN } from "../api_handlers/consts";
+    import { useContext, createContext, useState } from "react";
+    import { blankRequest } from "../api_handlers/session";
+    import { API_URL, LOGGED_IN } from "../api_handlers/consts";
 
-const AuthContext = createContext({
-    isAuthenticated: false,
-    setIsAuthenticated: () => {},
-});
+    const AuthContext = createContext({
+        isAuthenticated: false,
+        setIsAuthenticated: () => {},
+    });
 
-export function AuthProvider({children}) {
-    const [isAuthenticated, setIsAuthenticated] = useState(LOGGED_IN);
+    export function AuthProvider({children}) {
+        const [isAuthenticated, setIsAuthenticated] = useState(LOGGED_IN);
 
-    async function saveUser() {
-        await setIsAuthenticated(await blankRequest());
+        async function saveUser() {
+            await setIsAuthenticated(await blankRequest());
+        }
+
+        return (
+            <AuthContext.Provider value={{ isAuthenticated, saveUser }}>
+                {children}
+            </AuthContext.Provider>
+        );
     }
 
-    return (
-        <AuthContext.Provider value={{ isAuthenticated, saveUser }}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
-
-export const useAuth = () => useContext(AuthContext);
+    export const useAuth = () => useContext(AuthContext);
