e15d28c5b452a25afa4f9d160a35e10d4b87c2b8
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2024-12-07 19:02:14 +0100

    menu layout corrected for better ux with contact cards. Contact card component with props

--- src/components/chatComponents/xc-contact.jsx
+++ src/components/chatComponents/xc-contact.jsx
@@ -3,8 +3,15 @@ import default_profile_photo from "../../assets/profile_photos/default.png";
 import "../../styles/chat/chatComponents/s-xc-contactCard.css";
 
 export default function XcContact({
-    contactInformation,
-    lastMessage
+    contactInformation = {
+        profilePhoto: undefined,
+        name: "unknown",
+        username: "unknowun"
+    },
+    lastMessage = {
+        text: "...",
+        time: "xx:xx"
+    }
 }) {
     const ho = () => {
         console.log("hola");
@@ -13,16 +20,16 @@ export default function XcContact({
         <div className="xc-contact-container g-flex g-flex-gap16 g-pointer" onClick={ho}>
             <div className="g-flex g-horizontal-center-flex g-vertical-center-flex">
                 <img
-                    src={default_profile_photo}
+                    src={contactInformation.profilePhoto ? contactInformation.profilePhoto : default_profile_photo}
                     alt="oxapp default rpofile picture"
                     className="xc-contact-photo"
                 />
             </div>
             <div className="xc-contact-content g-flex g-flex-col">
-                <h3>Contacte0</h3>
+                <h3>{contactInformation.name}<span> ({contactInformation.username})</span></h3>
                 <div className="g-flex g-horizontal-spbtw-flex g-flex-gap20">
-                    <p className="g-flex-grow1">holass...</p>
-                    <p>10:20</p>
+                    <p className="xc-contact-content-lastMessage g-flex-grow1">{lastMessage.text}</p>
+                    <p>{lastMessage.time}</p>
                 </div>
             </div>
         </div>
@@ -30,5 +37,13 @@ export default function XcContact({
 }
 
 XcContact.propTypes = {
-
+    contactInformation: PropTypes.shape({
+        profilePhoto: PropTypes.string,
+        name: PropTypes.string,
+        username: PropTypes.string
+    }),
+    lastMessage: PropTypes.shape({
+        text: PropTypes.string,
+        time: PropTypes.string
+    })
 };
