cc94b8191026ee5b57feb7940fc03c2b288c379a
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2025-01-09 12:38:20 +0100

    ctrl enter for send message. clean text area on send

--- src/components/chatComponents/xc-messageInput.jsx
+++ src/components/chatComponents/xc-messageInput.jsx
@@ -1,58 +1,85 @@
-import { useEffect, useId } from "react";
+import { useRef, useEffect, useId } from "react";
 import { useTheme } from "../../theme/themeProvider";
+import PropTypes from "prop-types";
 
 import "../../styles/chat/chatComponents/s-xc-messageInput.css";
 
-export default function XcMessageInput({sendIcon, scrollToBottom, checkScroll, onSubmit, textareaRef}) {
-    const xc_messageInput_id = useId();
-    useEffect(() => {
-        const textarea = textareaRef.current;
-    
-        // Ajusta l'alçada i amplada automàticament
-        const handleInput = () => {
-            const scroll = checkScroll();
-    
-            // Ajusta l'alçada fins a un màxim de 200px
-            textarea.style.height = "auto"; // Reinicia l'alçada
-            const scrollHeight = Math.min(textarea.scrollHeight, 200); // Alçada màxima de 200px
-            textarea.style.height = `${scrollHeight}px`;
-    
-            if (scroll) scrollToBottom();
-        };
-    
-        textarea.addEventListener("input", handleInput);
-    
-        return () => {
-            textarea.removeEventListener("input", handleInput);
-        };
-    }, []);
+export default function XcMessageInput({
+  sendIcon,
+  scrollToBottom,
+  checkScroll,
+  onSubmit,
+}) {
+  const xc_messageInput_id = useId();
+  const textareaRef = useRef(null);
+  useEffect(() => {
+    const textarea = textareaRef.current;
 
-    const { icons } = useTheme();
+    // Ajusta l'alçada i amplada automàticament
+    const handleInput = () => {
+      const scroll = checkScroll();
 
-    function handleSend() {
-        onSubmit("hola");
-    }
+      // Ajusta l'alçada fins a un màxim de 200px
+      textarea.style.height = "auto"; // Reinicia l'alçada
+      const scrollHeight = Math.min(textarea.scrollHeight, 200); // Alçada màxima de 200px
+      textarea.style.height = `${scrollHeight}px`;
 
-    return (
-        <div className="xc-messageInput-contiainer g-flex g-horizontal-center-flex g-flex-gap20">
-            <div>
-                <img src={icons.messageTail} className="xc-message-tail" alt="message input tail" />
-                <textarea
-                    autoFocus={true}
-                    ref={textareaRef}
-                    name="textToSend"
-                    className="xc-messageInput"
-                    id={xc_messageInput_id}
-                    placeholder="Type a message"
-                    rows="1"
-                ></textarea>
-            </div>
-            <button
-                className="g-pointer xc-sendButton g-flex g-horizontal-center-flex g-vertical-center-flex"
-                onClick={handleSend}
-            >
-                <img src={sendIcon} alt="Send" />
-            </button>
-        </div>
-    );
+      if (scroll) scrollToBottom();
+    };
+
+    textarea.addEventListener("input", handleInput);
+
+    const handleKeyDown = (event) => {
+      if (event.ctrlKey && event.key === "Enter") {
+        handleSend();
+      }
+    };
+    textarea.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      textarea.removeEventListener("keydown", handleKeyDown);
+      textarea.removeEventListener("input", handleInput);
+    };
+  }, []);
+
+  const { icons } = useTheme();
+
+  function handleSend() {
+    onSubmit("hola");
+    textareaRef.current.value = "";
+  }
+
+  return (
+    <div className="xc-messageInput-contiainer g-flex g-horizontal-center-flex g-flex-gap20">
+      <div>
+        <img
+          src={icons.messageTail}
+          className="xc-message-tail"
+          alt="message input tail"
+        />
+        <textarea
+          autoFocus={true}
+          ref={textareaRef}
+          name="textToSend"
+          className="xc-messageInput"
+          id={xc_messageInput_id}
+          placeholder="Type a message"
+          rows="1"
+        ></textarea>
+      </div>
+      <button
+        className="g-pointer xc-sendButton g-flex g-horizontal-center-flex g-vertical-center-flex"
+        onClick={handleSend}
+      >
+        <img src={sendIcon} alt="Send" />
+      </button>
+    </div>
+  );
 }
+
+XcMessageInput.propTypes = {
+  sendIcon: PropTypes.string.isRequired,
+  scrollToBottom: PropTypes.func,
+  checkScroll: PropTypes.func,
+  onSubmit: PropTypes.func.isRequired,
+};
